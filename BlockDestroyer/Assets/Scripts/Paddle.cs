fb83a133335d7bed90e76af363f7718b7f734231
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-04-05 22:56:37 -0700

    added the play area which added the walls so that the ball wont leave the screen. Instead, the ball bouncess off of the walls. If the ball falls under the map, the scene will transition to my 'Game Over'. Also the block will disapear when the ball hits it

---
+++ BlockDestroyer/Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    // Configs
+    [SerializeField] float ScreenUnitsWide = 16f;
+    [SerializeField] float maxLimit = 15f;
+    [SerializeField] float minLimit = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * ScreenUnitsWide;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minLimit, maxLimit);
+        transform.position = paddlePos;
+	}
+}
