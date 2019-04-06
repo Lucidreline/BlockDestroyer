9d655e95d03455a6e77d595fc23bc6c8a9f5a57a
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-04-06 11:46:25 -0700

    Added an array of sounds. Each time the ball collides with something, it will randomly choose a sound froom the array and play it.

--- BlockDestroyer/Assets/Scripts/Ball.cs
+++ BlockDestroyer/Assets/Scripts/Ball.cs
@@ -8,16 +8,22 @@ public class Ball : MonoBehaviour {
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 2f;
+    [SerializeField] AudioClip[] ballSounds;
+
 
     private bool hasStarted = false;
 
+    // cached component refs
+    AudioSource MyAudioSource;
 
     Vector2 paddleToBallVector;
 
 	// Use this for initialization
 	void Start () {
         paddleToBallVector = transform.position - paddle1.transform.position;
-	}
+        MyAudioSource = GetComponent<AudioSource>();
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -45,5 +51,15 @@ public class Ball : MonoBehaviour {
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            MyAudioSource.PlayOneShot(clip);
+        }
+        
     }
 }
